feb21850db425ec753bdf9cc93bde5221d477ec6
Josh.Hintz <jhintz212@gmail.com>
2024-08-20 09:49:08 -0700

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,22 @@
+// Calculating Your Carbon Footprint - https://www.wikihow.com/Calculate-Your-Carbon-Footprint
+// Method 1
+
+// 1. How many members in my household?
+const householdMembers = 8
+// 2. What size is my house?
+const houseSize = 7
+// 3. What my food choice affect?
+const foodImpact = 6
+// 4. How much water do I consume?
+const waterConsumption = 1
+// 5. How many household gadgets do I purchase?
+const householdGadgets = 4
+// 6. How much waste do I produce?
+const wasteProduction = 20
+// 7. How much do I recycle?
+const recyclingAmount = 16
+// 8. Whats my transportation impact?
+const transportationImpact = 6
+
+// My Carbon Footprint Total
+const myCarbonTotal = 68
