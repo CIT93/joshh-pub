15d6124875c2d89603bfd84907d523910791d969
Josh.Hintz <jhintz212@gmail.com>
2024-08-21 14:29:45 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log['Hello World']
+console.log("hello from the inside the main.js file");
+
+// let myVar; 
+
+const myVar = "Josh";
+const myVarType = typeof(myVar);
+console.log("myVarType" + myVarType); //Old Way template strings
+console.log('myVarType ${myVarType}'); //New way template strings
+
+
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log('will 11 this one run?')
+    } else {
+        console.log('this 13 line will run')
+    }
+}
+
+runNow();
+runNow();
