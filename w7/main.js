198a665ee53c4686d4681509b6f71f53987e022b
Josh.Hintz <jhintz212@gmail.com>
2024-09-26 05:23:34 -0700

    challange 1

--- w7/main.js
+++ w7/main.js
@@ -1,47 +1,13 @@
 import { renderTbl } from "./render.js";
+import { determineHouseSizePts,determineHouseHoldPts} from "./carbonfp.js";
+
+
 
 const FORM = document.getElementById("form");
 const OUTPUT = document.getElementById("output");
 
 const cfpData = [];
 
-function determineHouseSizePts(size) {
-  let houseSizePoints = 0;
-  if (size === "large") {
-    houseSizePoints = 10;
-  } else if (size === "medium") {
-    houseSizePoints = 7;
-  } else if (size === "small") {
-    houseSizePoints = 4;
-  } else if (size === "apt") {
-    houseSizePoints = 2;
-  }
-  console.log(`Based on the size ${size}, the points are ${houseSizePoints},`);
-  return houseSizePoints;
-}
-
-function determineHouseHoldPts(numberInHousehold) {
-  let houseHoldPoints = 0;
-  if (numberInHousehold === 1) {
-    houseHoldPoints = 14;
-  } else if (numberInHousehold === 2) {
-    houseHoldPoints = 12;
-  } else if (numberInHousehold === 3) {
-    houseHoldPoints = 10;
-  } else if (numberInHousehold === 4) {
-    houseHoldPoints = 8;
-  } else if (numberInHousehold === 5) {
-    houseHoldPoints = 6;
-  } else if (numberInHousehold === 6) {
-    houseHoldPoints = 4;
-  } else if (numberInHousehold > 6) {
-    houseHoldPoints = 2;
-  }
-  console.log(
-    `Based on the household size ${numberInHousehold}, the points are ${houseHoldPoints}`
-  );
-  return houseHoldPoints;
-}
 
 function start(firstName, lastName, houseHoldMembers, houseSize) {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
