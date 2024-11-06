e6af17d2b7556ecc08194a17bdaa60ca466ff3d8
Josh.Hintz <jhintz212@gmail.com>
2024-11-06 13:36:04 -0800

    w13 my attempt at the code, before I review other students or solution code

--- w13/main.js
+++ w13/main.js
@@ -1,21 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("activityForm").addEventListener("submit", function (event) {
-        event.preventDefault();
+document.getElementById("activityForm").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        const outputDiv = document.getElementById("output");
-        const duration = parseInt(document.getElementById("duration").value, 10) * 1000;
+    const outputDiv = document.getElementById("output");
+    const duration = parseInt(document.getElementById("duration").value, 10) * 1000;
 
-        console.log("Form Submitted");
-        console.log("Duration:", duration);
+    console.log("Form Submitted");
+    console.log("Duration:", duration);
 
 
-        outputDiv.innerText = "Start Your Activity!"
-        outputDiv.classList.add("start");
+    outputDiv.innerText = "Start Your Activity!"
+    outputDiv.classList.add("start");
 
-        setTimeout(() => {
+    const timerPromise = new Promise((resolve, reject) => {
+        if (duration <= 0) {
+            reject("Duration must be greater then zero");
+        } else {
+            setTimeout(() => {
+                resolve("You're Done - Nice Job!");
+            }, duration);
+        }
+    })
+
+    timerPromise
+        .then(message => {
             outputDiv.innerText = "You're Done - Nice Job!"
             outputDiv.classList.remove("start");
             outputDiv.classList.add("stop");
-        }, duration);
-    });
+        })
+        .catch(error => {
+            outputDiv.innerText = "Try Again"
+            outputDiv.classList.remove("start");
+        });
 });
+
+    //     setTimeout(() => {
+    //         outputDiv.innerText = "You're Done - Nice Job!"
+    //         outputDiv.classList.remove("start");
+    //         outputDiv.classList.add("stop");
+    //     }, duration);
+    // });
